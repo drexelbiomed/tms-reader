dc9b2b274e02751a5b378fcbf62caab1092fdefa
Carlos Diaz <cdiaz86@gmail.com>
2019-10-09 10:30:03 -0400

    Updates README.md & outputs all UG online courses.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # TMS Reader
 
 Reads Term Master Schedule spreadsheet and outputs select courses.
+
+# To-do
+
+Set up output to spreadsheet.
--- tms-reader.rb
+++ tms-reader.rb
@@ -1,11 +1,14 @@
 require 'csv'
 
+
 def tms_read
+  courses = Hash.new
   CSV.foreach('data/STU-Term Master schedule_all_2019.csv', headers: true) do |header|
-    if header["Sess Desc"] == "Online"
-      p "#{header["Course Title"]} (#{header["Course"]}) is offered in #{header["Term Code"]} and online. The CRN is #{header["Crn"]}."
+    if header["Sess Desc"] == "Online" && header["Crse Numb"].to_i < 500
+      p "#{header["Term Code"]},#{header["Course"]},#{header["Course Title"]},#{header["Credit Hrs"]},#{header["Crn"]}"
     end
   end
+  courses
 end
 
-puts tms_read
+p tms_read
